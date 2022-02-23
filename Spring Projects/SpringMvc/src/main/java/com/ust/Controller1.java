d2e9dd9914a33dc4488544df58cb8bc20a07cf83
Adithya K Prabhu <66137659+adithyakprabhu@users.noreply.github.com>
2022-02-23 18:24:44 +0530

    Add files via upload

---
+++ Spring Projects/SpringMvc/src/main/java/com/ust/Controller1.java
@@ -0,0 +1,15 @@
+package com.ust;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Controller1 {
+	
+	@RequestMapping("/")
+	public String displayInfo()
+	{
+		return "Stringmvc";
+	}
+
+}
